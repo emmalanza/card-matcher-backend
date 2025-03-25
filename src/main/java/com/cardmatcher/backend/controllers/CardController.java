bef9ee27cad0d160f6004ebd5af8b0b937e23f04
Emma <emmalanza@hotmail.com>
2025-03-25 11:46:21 +0100

    cors config + get card from api

--- src/main/java/com/cardmatcher/backend/controllers/CardController.java
+++ src/main/java/com/cardmatcher/backend/controllers/CardController.java
@@ -1,9 +1,13 @@
 package com.cardmatcher.backend.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cardmatcher.backend.models.Card;
 import com.cardmatcher.backend.services.CardService;
 
 @RestController
@@ -20,6 +24,11 @@ public class CardController {
     public String loadCards() {
         cardService.loadCardsFromAPI();
         return "Cards loaded!";
+    }
+
+     @GetMapping("/api/cards")
+    public List<Card> getCardsBySetId(@RequestParam String setId) {
+        return cardService.getCardsBySetId(setId);
     }
 
 }
