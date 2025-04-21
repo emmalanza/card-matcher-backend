22e37c941abde3f46f39a584e0f501008202e852
Emma <emmalanza@hotmail.com>
2025-04-21 09:07:29 +0200

    get user details without roles

--- src/main/java/com/cardmatcher/backend/config/SecurityConfig.java
+++ src/main/java/com/cardmatcher/backend/config/SecurityConfig.java
@@ -3,55 +3,61 @@ package com.cardmatcher.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.cardmatcher.backend.security.JwtFilter;
 import com.cardmatcher.backend.services.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
     private final CustomUserDetailsService customUserDetailsService;
 
-    // Inyectar el servicio de UserDetails personalizado
-    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+    public SecurityConfig(JwtFilter jwtFilter, CustomUserDetailsService customUserDetailsService) {
+        this.jwtFilter = jwtFilter;
         this.customUserDetailsService = customUserDetailsService;
     }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())  
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**").permitAll() 
-                .anyRequest().permitAll() 
-            );
-        return http.build();
-    }
-
-
     // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-    // Exception {
-    // http
-    // .csrf(csrf -> csrf.disable())
-    // .cors(cors -> cors.disable())
-    // .authorizeHttpRequests(auth -> auth
-    // .requestMatchers("/api/auth/**").permitAll() // login and register public
-    // .requestMatchers("/api/cards/**").permitAll() // get cards public
-    // .requestMatchers("/api/user/**").authenticated() // user actions protected
-    // .anyRequest().authenticated() // all other requests protected
-    // )
-    // .sessionManagement(sess ->
-    // sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-    // return http.build();
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         .csrf(csrf -> csrf.disable())  
+    //         .authorizeHttpRequests(auth -> auth
+    //             .requestMatchers("/**").permitAll() 
+    //             .anyRequest().permitAll() 
+    //         );
+    //     return http.build();
     // }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+    Exception {
+    http
+    .csrf(csrf -> csrf.disable())
+    .cors(Customizer.withDefaults())
+    .authorizeHttpRequests(auth -> auth
+    .requestMatchers("/api/auth/**").permitAll() // login and register public
+    .requestMatchers("/api/cards/**").permitAll() // get cards public
+    .requestMatchers("/api/users/**").authenticated() // user actions protected
+    .anyRequest().authenticated() // all other requests protected
+    )
+    .sessionManagement(sess ->
+    sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+    
+    }
 
     @Bean
     public AuthenticationManager authenticationManagerBean(HttpSecurity http) throws Exception {
