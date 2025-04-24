fd11171ad7f01901107a3b28738d2bd8b4c41d37
Emma <emmalanza@hotmail.com>
2025-04-24 20:47:36 +0200

    get lists by user

--- src/main/java/com/cardmatcher/backend/controllers/CardListController.java
+++ src/main/java/com/cardmatcher/backend/controllers/CardListController.java
@@ -1,13 +1,19 @@
 package com.cardmatcher.backend.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cardmatcher.backend.models.User;
+import com.cardmatcher.backend.repositories.UserRepository;
 import com.cardmatcher.backend.services.CardListService;
 
 @RestController
@@ -16,6 +22,9 @@ public class CardListController {
 
     @Autowired
     private CardListService cardListService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     // Añadir una carta a una lista
     @PostMapping("/{cardListId}/cards/{cardId}")
@@ -34,4 +43,20 @@ public class CardListController {
         cardListService.removeCardFromCardList(cardListId, cardId);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/user")
+    public  ResponseEntity<?> getUserCardLists(@AuthenticationPrincipal UserDetails userDetails) {
+        if (userDetails == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuario no autenticado");
+        }
+
+        String username = userDetails.getUsername();
+        System.out.println("Usuario encontrado: " + username);
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        return ResponseEntity.ok(cardListService.getCardListsByUser(user));
+    }
+
 }
--- src/main/java/com/cardmatcher/backend/services/CardListService.java
+++ src/main/java/com/cardmatcher/backend/services/CardListService.java
@@ -1,39 +1,46 @@
 package com.cardmatcher.backend.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cardmatcher.backend.models.Card;
 import com.cardmatcher.backend.models.CardList;
+import com.cardmatcher.backend.models.User;
 import com.cardmatcher.backend.repositories.CardListRepository;
 import com.cardmatcher.backend.repositories.CardRepository;
 
 @Service
 public class CardListService {
 
-    @Autowired
-    private CardListRepository cardListRepository;
+        @Autowired
+        private CardListRepository cardListRepository;
 
-    @Autowired
-    private CardRepository cardRepository;
+        @Autowired
+        private CardRepository cardRepository;
 
-    public void addCardToCardList(Long cardListId, String cardId) {
-        CardList cardList = cardListRepository.findById(cardListId)
-                .orElseThrow(() -> new RuntimeException("Lista no encontrada"));
-        Card card = cardRepository.findById(cardId)
-                .orElseThrow(() -> new RuntimeException("Carta no encontrada"));
+        public void addCardToCardList(Long cardListId, String cardId) {
+                CardList cardList = cardListRepository.findById(cardListId)
+                                .orElseThrow(() -> new RuntimeException("Lista no encontrada"));
+                Card card = cardRepository.findById(cardId)
+                                .orElseThrow(() -> new RuntimeException("Carta no encontrada"));
 
-        cardList.getCards().add(card);
-        cardListRepository.save(cardList);
-    }
+                cardList.getCards().add(card);
+                cardListRepository.save(cardList);
+        }
 
-    public void removeCardFromCardList(Long cardListId, String cardId) {
-        CardList cardList = cardListRepository.findById(cardListId)
-                .orElseThrow(() -> new RuntimeException("Lista no encontrada"));
-        Card card = cardRepository.findById(cardId)
-                .orElseThrow(() -> new RuntimeException("Carta no encontrada"));
+        public void removeCardFromCardList(Long cardListId, String cardId) {
+                CardList cardList = cardListRepository.findById(cardListId)
+                                .orElseThrow(() -> new RuntimeException("Lista no encontrada"));
+                Card card = cardRepository.findById(cardId)
+                                .orElseThrow(() -> new RuntimeException("Carta no encontrada"));
 
-        cardList.getCards().remove(card);
-        cardListRepository.save(cardList);
-    }
+                cardList.getCards().remove(card);
+                cardListRepository.save(cardList);
+        }
+
+        public List<CardList> getCardListsByUser(User user) {
+                return cardListRepository.findByUser(user);
+        }
 }
